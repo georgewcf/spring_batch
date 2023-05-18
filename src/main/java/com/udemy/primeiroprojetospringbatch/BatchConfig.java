9e5e88b177215d8cb3115eb0e83b5864a032c2ab
George Ferreira <gehwallace@gmail.com>
2023-05-17 22:26:50 -0300

    Imprimindo o valor atraves de parametros

--- src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
+++ src/main/java/com/udemy/primeiroprojetospringbatch/BatchConfig.java
@@ -1,16 +1,18 @@
 package com.udemy.primeiroprojetospringbatch;
 
-import lombok.Data;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -24,24 +26,33 @@ public class BatchConfig {
     @Autowired
     private StepBuilderFactory steBuilderFactory;
     @Bean
-    public Job imprimeOlaJob(){
+    public Job getHelloJob(){
 
         return jobBuilderFactory
                 .get("IMPRIMINDO O METODO 'OLÁ JOB'")
-                .start(imprimeOlaStep())
+                .start(getHelloStep())
+                .incrementer(new RunIdIncrementer())
+                //TOMAR CUIDADO AO USAR O INCREMENTER. SE O JOB PARAR, ELE N RESTARTAR DE ONDE PAROU
+                //ELE VAI CRIAR OUTRA INSTANCIA DO ZERO
                 .build();
     }
 
-    private Step imprimeOlaStep() {
+    private Step getHelloStep() {
         return steBuilderFactory
                 .get("IMPRIMINDO O METODO 'OLÁ STEP'")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Olá Mundo!");
-                        return RepeatStatus.FINISHED; //tarefa terminada
-                    }
-                })
+                .tasklet(getTasklet(null))
                 .build();
+    }
+
+    @Bean
+    @StepScope
+    public Tasklet getTasklet(@Value("#{jobParameters['nome']}") String nome) {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println("Olá Mundo!");
+                return RepeatStatus.FINISHED; //tarefa terminada
+            }
+        };
     }
 }
